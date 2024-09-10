5f836914f1859e14d216b7fc4c189051518cda8f
Prathamesh Chougale <prathamesh17170@gmail.com>
2024-09-10 14:58:11 +0530

    refactor: Remove unnecessary animation from AboutPage

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,21 +1,10 @@
-"use client";
-
 import React from "react";
-import { motion } from "framer-motion";
 import IntroSection from "./_components/intro";
 import TeamSection from "./_components/team";
 
 const AboutPage = () => {
   return (
     <div className="container mx-auto z-20 px-4 py-8">
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
-      >
-        <h1 className="text-4xl font-bold mb-8">About Us</h1>
-      </motion.div>
-
       <div className="space-y-12">
         <IntroSection />
       </div>
